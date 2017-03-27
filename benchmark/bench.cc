e62857b8122eda36e2dd72b05c1fe113086f63b3
foudrer <foudrer@163.com>
2017-03-27 14:28:03 +0800

    function define

---
+++ benchmark/bench.cc
@@ -0,0 +1,5 @@
+#include "popcount.h"
+
+int main() {
+	return 0;
+}
---
+++ include/popcount.h
@@ -0,0 +1,21 @@
+#ifndef POPCOUNT_H
+#define POPCOUNT_H
+
+#include <iostream>
+#include <nmmintrin.h>
+#include <time.h>
+
+uint32_t direct_popcount_32(uint32_t num);
+uint32_t directop_popcount_32(uint32_t num);
+uint32_t fast_popcount_32(uint32_t num);
+uint32_t parallel_popcount_32(uint32_t num);
+uint32_t cpu_popcount_32(uint32_t num);
+
+uint64_t direct_popcount_64(uint64_t num);
+uint64_t directop_popcount_64(uint64_t num);
+uint64_t fast_popcount_64(uint64_t num);
+uint64_t parallel_popcount_64(uint64_t num);
+uint64_t cpu_popcount_64(uint64_t num);
+
+#endif
+
